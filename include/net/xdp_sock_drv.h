0751f428830cc7721bd155d0b7046a7cb69a063d
Yibo Wang <2024312277@stu.hit.edu.cn>
2026-07-01 01:12:55 +0800

    patch

--- include/net/xdp_sock_drv.h
+++ include/net/xdp_sock_drv.h
@@ -137,7 +137,7 @@ static inline void xsk_buff_free(struct xdp_buff *xdp)
 		goto out;
 
 	list_for_each_entry_safe(pos, tmp, xskb_list, xskb_list_node) {
-		list_del_init(&frag->list_node);
+		list_del_init(&pos->xskb_list_node);
 		xp_free(pos);
 	}
 
@@ -162,7 +162,7 @@ static inline struct xdp_buff *xsk_buff_get_frag(struct xdp_buff *first)
 	frag = list_first_entry_or_null(&xskb->pool->xskb_list,
 					struct xdp_buff_xsk, xskb_list_node);
 	if (frag) {
-		list_del_init(&frag->list_node);
+		list_del_init(&frag->xskb_list_node);
 		ret = &frag->xdp;
 	}
 
@@ -173,7 +173,7 @@ static inline void xsk_buff_del_tail(struct xdp_buff *tail)
 {
 	struct xdp_buff_xsk *xskb = container_of(tail, struct xdp_buff_xsk, xdp);
 
-	list_del_init(&frag->list_node);
+	list_del_init(&xskb->xskb_list_node);
 }
 
 static inline struct xdp_buff *xsk_buff_get_tail(struct xdp_buff *first)
